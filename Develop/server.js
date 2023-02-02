e99be84509b662fae955b17ea100866842e2030b
kflinnn <kflinncoding@gmail.com>
2023-02-02 14:50:21 -0500

    added uuid, updated get/post requests

--- Develop/server.js
+++ Develop/server.js
@@ -2,7 +2,8 @@
 const express = require('express');
 const path = require('path');
 const { clog } = require('./middleware/clog');
-const api = require('./public/assets/js/index');
+//Helper method for generating unique ids
+const uuid = require('./helpers/uuid');
 
 //Require the JSON file 
 const noteData = require("./db/db.json");
@@ -15,7 +16,7 @@ app.use(clog);
 //Middleware to parse JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
+
 
 app.use(express.static('public'));
 
@@ -31,10 +32,53 @@ app.get('*', (req, res) =>
 );
 
 //API route that returns all saved notes as JSON
-app.get('/api/notes', (req, res) => res.json(noteData));
+app.get('/api/notes', (req, res) => {
+    console.info(`GET /api/notes`);
+    res.status(200).json(noteData);
+});
 
+//GET a single note
 
+app.get('/api/notes/:notes_id', (req, res) => {
+    if (req.params.notes_id) {
+      console.info(`${req.method} request received to get a single a review`);
+      const noteId = req.params.notes_id;
+      for (let i = 0; i < noteData.length; i++) {
+        const currentNote = noteData[i];
+        if (currentNote.notes_id === noteId) {
+          res.json(currentNote);
+          return;
+        }
+      }
+      res.status(404).send('Review not found');
+    } else {
+      res.status(400).send('Review ID not provided');
+    }
+  });
+
+//Post request to add a note 
+app.post('/api/notes', (req, res) => {
+    console.info(`${req.method} request received to add a note`);
+    const { title, text } = req.body;
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            notes_id: uuid(),
+        };
+
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+       
+      console.log(resposne);
+      res.status(201).json(response);  
+    } else {
+        res.status(500).json('Error in posting note');
+    }
+});
 
 app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
+  console.log(`App listening at http://localhost:${PORT}`)
 );
